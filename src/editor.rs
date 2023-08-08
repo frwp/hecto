a0d377ce21ae6711f2af7ac8b62f7bd3aa782b17
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2023-08-08 08:22:09 +0700

    add draw rows when run

--- src/editor.rs
+++ src/editor.rs
@@ -43,8 +43,17 @@ impl Editor {
         print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
         if self.should_quit {
             println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            print!("{}", termion::cursor::Goto(3, 1));
         }
         std::io::stdout().flush()
+    }
+
+    fn draw_rows(&self) {
+        for _ in 0..24 {
+            println!("~\r");
+        }
     }
 }
 
