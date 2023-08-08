aa85168f1866283488d176447b171896ff2f4ecf
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2023-08-08 11:03:08 +0700

    add cursor movement with  Home, End, PgUp, PgDwn

--- src/editor.rs
+++ src/editor.rs
@@ -43,7 +43,14 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
-            Key::Up | Key::Down | Key::Left | Key::Right => self.move_cursor(pressed_key),
+            Key::Up
+            | Key::Down
+            | Key::Left
+            | Key::Right
+            | Key::Home
+            | Key::End
+            | Key::PageUp
+            | Key::PageDown => self.move_cursor(pressed_key),
             _ => (),
         }
         Ok(())
@@ -81,6 +88,10 @@ impl Editor {
                     x = x.saturating_add(1)
                 }
             }
+            Key::Home => x = 0,
+            Key::End => x = width,
+            Key::PageUp => y = 0,
+            Key::PageDown => y = height,
             _ => (),
         }
         self.cursor_position = Position { x, y }
